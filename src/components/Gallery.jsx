98d47ef32b02a270e48e3060ea3298805ea0b586
AnukoolFS1 <ascmailing0@gmail.com>
2024-08-17 10:48:00 +0530

    added a gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -13,10 +13,10 @@ const Gallery = () => {
             <article className='projects'>
                 <div>
                     <a href="https://66bf9e99c248989016946bd2--papaya-cannoli-f75b68.netlify.app/" target='_blank'>  Static Web Page  </a>
-                        <iframe id='gpPage' src="../src/BootstrapMadeGP/index.html" frameborder="0" height='700px' width='700px'></iframe>
+                        <iframe id='gpPage' src="https://66bf9e99c248989016946bd2--papaya-cannoli-f75b68.netlify.app/" frameborder="0" height='700px' width='700px'></iframe>
                 </div>
                 <div>
-                    <label> Calculator </label>
+                    <a href="https://kaleidoscopic-halva-853976.netlify.app/"> Calculator </a>
                     <iframe src="https://kaleidoscopic-halva-853976.netlify.app/" frameborder="0" height='700px' width='700px'></iframe>
                 </div>
             </article>

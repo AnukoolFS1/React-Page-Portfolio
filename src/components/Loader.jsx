7ab2ebb8c237b74769651c48a1e906f3fd8487b7
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-08-28 15:06:27 +0530

    changed loader for Edge

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,15 +1,14 @@
 import '../Css/loader.css'
 
-const Loader = ({display}) => {
+const Loader = ({ display }) => {
 
 
     return (
-        <div className='loader' style={{display:display}} >
-            <div className='first'></div>
-            <div className='second'></div>
-            <div className='visible'></div>
-            <div className='fourth'></div>
-            <div className='fifth'></div>
+        <div className='loaderedge' style={{display:display}}>
+            <div className='hider'>
+                <div></div>
+                <div></div>
+            </div>
         </div>
     )
 }

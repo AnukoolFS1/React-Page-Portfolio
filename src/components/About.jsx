2e45d999667aabebdbc3d1f99478e251649f3341
Anukool Singh Chauhan <ascmailing0@gmail.com>
2024-08-29 10:53:14 +0530

    added some resume part

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,29 +1,30 @@
 import { useState } from 'react'
 import Img from '../assets/images/chairs.jpg'
 import '../Css/about.css'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
+import 'boxicons'
 
 
 const About = () => {
     let [zIn, setZIn] = useState(false)
-    const navigate = useNavigate() 
+    const navigate = useNavigate()
 
-    function zUp(){ // setting z-index up for about pages
+    function zUp() { // setting z-index up for about pages
         setZIn(true)
     }
 
-    function zDown(){ // setting z-index down for about pages
+    function zDown() { // setting z-index down for about pages
         setZIn(false)
     }
 
 
     return (
         <section className='abouts'>
-                {/** About front page */}
-            <div className='heading1' onClick={zDown} 
-            style={{
-                top: zIn?'200px': '120px', 
-                backgroundColor: zIn?'#0f2952':'#1e2735' /*-- setting page animation for changes --*/
+            {/** About front page */}
+            <div className='heading1' onClick={zDown}
+                style={{
+                    top: zIn ? '200px' : '120px',
+                    backgroundColor: zIn ? '#0f2952' : '#1e2735' /*-- setting page animation for changes --*/
                 }}>
                 <div className='abt-cntnt1'>
                     <h1 className='abt-hding com-h1 ab-h1'>About</h1>
@@ -41,17 +42,88 @@ const About = () => {
                 </article>
             </div>
 
-                {/** About rear page */}
-            <div className='heading2' 
-            style={{ 
-                zIndex: zIn?3:1, 
-                top: zIn?'120px': '200px', 
-                backgroundColor: zIn?'#1e2735':'#0f2952' /*-- setting page animation for changes --*/
-                }} 
-                onClick={zUp}> 
-                <h1 className='abt-hding com-h1 ab-h1'>About</h1>
-                <p className='com-p'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis voluptatibus ut odit reprehenderit distinctio doloribus? Doloremque aspernatur necessitatibus, sit perspiciatis maiores cupiditate velit ut deserunt omnis atque quae suscipit alias?</p>
-                <button className='Cntct-btn' onClick={()=>navigate('/contacts')}>Contact me</button>
+            {/** About rear page */}
+            <div className='heading2'
+                style={{
+                    zIndex: zIn ? 3 : 1,
+                    top: zIn ? '120px' : '200px',
+                    backgroundColor: zIn ? '#1e2735' : '#0f2952' /*-- setting page animation for changes --*/
+                }}
+                onClick={zUp}>
+                <h1 className='abt-hding com-h1 ab-h1'>Resume</h1>
+                <h1 className='title'>Anukool Singh Chauhan</h1>
+                <h2>Frontend Developer</h2>
+                <article>
+                    <p>Fatehpur, Beri</p>
+                    <p>New Delhi</p>
+                    <p><strong>9560095435</strong></p>
+                    <p><strong>anukool26@outlool.com</strong></p>
+                </article>
+                <section className='table'>
+                    <section className='col1'>
+                        <article>
+                            <h3>Experience</h3>
+                            <p>Fresher</p>
+                        </article>
+                        <article>
+                            <h3>Education</h3>
+                            <div>
+                                <p>Sarvodya Bal Vidyalaya, <span>Fatehpur Beri</span></p>
+                                <h4>10<sup>th</sup> Class <span>~ Apr 2013 - Mar 2014</span></h4>
+                            </div>
+                            <div>
+                                <p>Dice Academy, <span>Shiekh Sarai</span></p>
+                                <h4>Full Stack Development <span>~ Dec 2023 - Present</span></h4>
+                            </div>
+                        </article>
+                        <article>
+                            <h3 onClick={() => navigate('/gallery')}>Projects</h3>
+                            <Link to={'/'}>Portfolio</Link>
+                            <Link to={'/gallery#static'}>Static Web Page</Link>
+                            <Link to={'/gallery#calc'}>Calculator</Link>
+                        </article>
+                        <article>
+                            <h3>
+                                Online Presence
+                            </h3>
+                        </article>
+                    </section>
+                    <section className='col2'>
+                        <article>
+                            <h3>Skills</h3>
+                            <p>Frontend Development</p>
+                            <h4>
+                                <span><box-icon type='logo' name='html5' color='#ccff00'></box-icon></span> HTML
+                            </h4>
+                            <h4>
+                                <span><box-icon name='css3' type='logo' color='#ccff00' ></box-icon></span> CSS
+                            </h4>
+                            <h4>
+                                <span><box-icon name='javascript' type='logo' color='#ccff00'></box-icon></span> JAVASCRIPT </h4>
+                            <h4>
+                                <span><box-icon type='logo' name='bootstrap' color='#ccff00'></box-icon></span> Bootstrap
+                            </h4>
+                            <h4>
+                                <span><box-icon name='devices' color='#ccff00'></box-icon></span> Responsive
+                            </h4>
+                            <h4>
+                                <span><box-icon name='react' type='logo' color='#ccff00'></box-icon></span> React
+                            </h4>
+                            <p>Version Control</p>
+                            <h4>
+                                <span><box-icon name='git' type='logo' color='#ccff00'></box-icon></span> Git
+                            </h4>
+                            <h4>
+                                <span><box-icon name='github' type='logo' color='#ccff00'></box-icon></span> Github
+                            </h4>
+                        </article>
+                        <article>
+                            <h3>Languages</h3>
+                            <h4>English, Hindi</h4>
+                        </article>
+                    </section>
+                </section>
+                <button className='Cntct-btn' onClick={() => navigate('/contacts')}>Contact me</button>
 
             </div>
 
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -11,11 +11,11 @@ const Gallery = () => {
 
             <h3>Projects</h3>
             <article className='projects'>
-                <div>
+                <div id='static'>
                     <a href="https://66bf9e99c248989016946bd2--papaya-cannoli-f75b68.netlify.app/" target='_blank'>  Static Web Page  </a>
                     <iframe id='gpPage' src="https://66bf9e99c248989016946bd2--papaya-cannoli-f75b68.netlify.app/" frameborder="0" height='700px' width='700px'></iframe>
                 </div>
-                <div>
+                <div id='calc'>
                     <a href="https://kaleidoscopic-halva-853976.netlify.app/"> Calculator </a>
                     <iframe src="https://kaleidoscopic-halva-853976.netlify.app/" frameborder="0" height='700px' width='700px'></iframe>
                 </div>
